d3b4dc71d80397a62120f5972e23241b64e86529
TechnoDot <techdudie@mail.com>
2025-09-06 22:13:14 -0500

    make shit actually push

---
+++ TeamCode2026/src/main/java/com/mech/ftc/twentyfive/BaboOS.java
@@ -0,0 +1,12 @@
+package com.mech.ftc.twentyfive;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="BaboOS", group="TechnoCode")
+public class BaboOS extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        // had to put this file here so shit would push, change it
+    }
+}
