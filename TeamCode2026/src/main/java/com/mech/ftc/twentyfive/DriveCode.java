790bd39796faa688bae5414270f03f4673b38012
SM-mech <jakewinfield94@gmail.com>
2025-09-06 22:28:46 -0500

    Renamed teleop

--- TeamCode2026/src/main/java/com/mech/ftc/twentyfive/DriveCode.java
+++ TeamCode2026/src/main/java/com/mech/ftc/twentyfive/DriveCode.java
@@ -3,10 +3,10 @@ package com.mech.ftc.twentyfive;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name="BaboOS", group="TechnoCode")
-public class BaboOS extends LinearOpMode {
+@TeleOp(name="DriveCode")
+public class DriveCode extends LinearOpMode {
     @Override
     public void runOpMode() {
-        // had to put this file here so shit would push, change it
+
     }
 }
