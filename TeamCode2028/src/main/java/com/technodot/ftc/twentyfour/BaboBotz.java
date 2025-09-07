adc8e0ca317035c2d99cbcdd0dc5e22d115525fd
TechnoDot <techdudie@mail.com>
2025-09-06 21:09:00 -0500

    import code from last year

---
+++ TeamCode2028/src/main/java/com/technodot/ftc/twentyfour/BaboBotz.java
@@ -0,0 +1,79 @@
+package com.technodot.ftc.twentyfour;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@SuppressWarnings("FieldCanBeLocal")
+@TeleOp(name="BaboBotz", group="TechnoCode")
+public class BaboBotz extends LinearOpMode {
+
+    private DcMotor driveRight;
+    private DcMotor driveLeft;
+    private DcMotor slideMain;
+    private DcMotor clawArm;
+    private Servo clawMain;
+
+    public float driveSpeedMultiplier = 1.0F;
+    public long slideTimer = 0;
+    public static final int driveRetreatTimer = 900;
+
+    @Override
+    public void runOpMode() {
+        driveRight = hardwareMap.get(DcMotor.class, "driveRight");
+        driveLeft = hardwareMap.get(DcMotor.class, "driveLeft");
+        slideMain = hardwareMap.get(DcMotor.class, "slideMain");
+        clawArm = hardwareMap.get(DcMotor.class, "clawArm");
+        clawMain = hardwareMap.get(Servo.class, "clawMain");
+
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (gamepad1.right_trigger > 0.5) {
+                    driveSpeedMultiplier = 0.5F;
+                } else {
+                    driveSpeedMultiplier = 1.0F;
+                }
+
+                driveLeft.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x) * driveSpeedMultiplier);
+                driveRight.setPower(-(gamepad1.left_stick_y + gamepad1.left_stick_x) * driveSpeedMultiplier);
+
+                if (gamepad1.left_trigger > 0.5 && slideTimer == 0) {
+                    slideTimer = System.currentTimeMillis();
+                }
+
+                if (System.currentTimeMillis() < slideTimer + driveRetreatTimer) {
+                    driveLeft.setPower(0.8);
+                    driveRight.setPower(-0.8);
+                } else {
+                    slideTimer = 0;
+                }
+
+                if (gamepad1.left_bumper) {
+                    slideMain.setPower(-1);
+                } else if (gamepad1.right_bumper) {
+                    slideMain.setPower(1);
+                } else {
+                    slideMain.setPower(0);
+                }
+
+                if (gamepad1.y) {
+                    clawArm.setPower(0.4);
+                } else if (gamepad1.x) {
+                    clawArm.setPower(-0.4);
+                } else {
+                    clawArm.setPower(0);
+                }
+
+                if (gamepad1.a) {
+                    clawMain.setPosition(0);
+                } else if (gamepad1.b) {
+                    clawMain.setPosition(1);
+                }
+
+                telemetry.update();
+            }
+        }
+    }
+}
---
+++ TeamCode2028/src/main/java/com/technodot/ftc/twentyfour/README.md
@@ -0,0 +1,46 @@
+# BaboOS
+
+This repository contains [the code](https://github.com/TechDudie/BaboOS/tree/main/TeamCode/src/main/java/com/technodot/ftc/twentyfour) for Shawnee Mission East Robotics Two #21692 INTO THE DEEP (2024-2025) competition season.
+
+### Helpful Crap
+
+requires Android Studio 2024.2.1 Ladybug + Java 21<br>
+TeleOp at `TeamCode/src/main/java/com.technodot.ftc.twentyfour/BaboOS.java`<br>
+Auto at `TeamCode/src/main/java/com.technodot.ftc.twentyfour/BaboAuto.java`<br>
+
+### People
+*in order of usefulness*
+*specifically people who have actually done stuff and have been useful, seeeed.*
+
+- **@akwatson** the head builder/thinker
+- **@technodot** the programmer/unbuilder/unthinker/major problem
+- **@silentrunner101** the designer/builder
+- **@gavinlasky** the builder/thinker without discord
+- **@sheeparmy** the silly builder/thinker
+- **@somethin_999** the builder/thinker
+- **@safire1k** the builder/thinker
+- **@PumkinCat-Java** the other programmer/builder (the one that isn't a problem)
+- **@cargarzarwild** the experienced yet missing sophomore
+
+### Controls
+
+- **Left Joystick** - Movement (depends on selected modes)
+- **Right Joystick** - Movement (depends on selected modes)
+- **Left Trigger** - Precise Mode
+- **Right Trigger** - Raise Claw Arm (Force)
+- **Left Bumper** - Extend Slide
+- **Right Bumper** - Retract Slide
+- **A Button** - Open Claw
+- **B Button** - Close Claw
+- **X Button** - Lower Claw Arm
+- **Y Button** - Raise Claw Arm
+- **D-Pad Up** - Extend Slide (Override)
+- **D-Pad Down** - Lower Slide (Override)
+- **D-Pad Left** - Enable Camera Processing
+- **D-Pad Right** - Disable Camera Processing
+
+### Results
+*League: **KCW***
+```
+2024-2025: 21st (1.06 8.19 5.63 141 8-7-1 24) Control Award 2nd Place
+```
