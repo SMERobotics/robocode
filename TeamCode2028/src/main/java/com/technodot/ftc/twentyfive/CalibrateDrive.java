7156b49e5b23f754371dd62b041bd917223a33f1
TechDudie <techdudie@mail.com>
2025-11-07 14:53:48 -0600

    @TechDudie: flip direction for ts br motor on CalibrateDrive

--- TeamCode2028/src/main/java/com/technodot/ftc/twentyfive/CalibrateDrive.java
+++ TeamCode2028/src/main/java/com/technodot/ftc/twentyfive/CalibrateDrive.java
@@ -36,7 +36,8 @@ public class CalibrateDrive extends OpMode {
         motorFrontLeft.setDirection(DcMotorEx.Direction.FORWARD);
         motorFrontRight.setDirection(DcMotorEx.Direction.FORWARD);
         motorBackLeft.setDirection(DcMotorEx.Direction.REVERSE);
-        motorBackRight.setDirection(DcMotorEx.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorEx.Direction.FORWARD); // ts should be reverse. CHAT MOUNTED IT BACKWARDS WHAT THE FUU
+//        motorBackRight.setDirection(DcMotorEx.Direction.REVERSE);
     }
 
     public void update(Gamepad gamepad) {
