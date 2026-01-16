8f33918c96e4af098bb32ea97248d50285bae870
technodot <techdudie@mail.com>
2026-01-15 20:03:15 -0600

    qol calib opmode updates

--- TeamCode2028/src/main/java/org/technodot/ftc/twentyfivebeta/tests/CalibrateColorSensors.java
+++ TeamCode2028/src/main/java/org/technodot/ftc/twentyfivebeta/tests/CalibrateColorSensors.java
@@ -5,15 +5,20 @@ import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+import org.technodot.ftc.twentyfivebeta.common.Alliance;
 import org.technodot.ftc.twentyfivebeta.robocore.DeviceIntake;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.technodot.ftc.twentyfivebeta.roboctrl.SilentRunner101;
 
 import java.util.HashMap;
 
 @TeleOp(name="CalibrateColorSensors", group="TechnoCode")
 public class CalibrateColorSensors extends OpMode {
+
+    public DeviceIntake deviceIntake;
 
     public RevColorSensorV3 colorLeft1;
     public RevColorSensorV3 colorLeft2;
@@ -27,6 +32,9 @@ public class CalibrateColorSensors extends OpMode {
     @Override
     public void init() {
         this.t = FtcDashboard.getInstance().getTelemetry();
+
+        deviceIntake = new DeviceIntake(Alliance.BLUE);
+        deviceIntake.init(hardwareMap, new SilentRunner101(gamepad1, gamepad2));
 
         colorLeft1 = hardwareMap.get(RevColorSensorV3.class, "colorLeft1");
         colorLeft2 = hardwareMap.get(RevColorSensorV3.class, "colorLeft2");
@@ -42,6 +50,8 @@ public class CalibrateColorSensors extends OpMode {
 
     @Override
     public void start() {
+        deviceIntake.start();
+
         if (colorLeft1 != null) colorSensors.put("cl1", colorLeft1);
         if (colorLeft2 != null) colorSensors.put("cl2", colorLeft2);
         if (colorRight1 != null) colorSensors.put("cr1", colorRight1);
@@ -53,6 +63,8 @@ public class CalibrateColorSensors extends OpMode {
 
     @Override
     public void loop() {
+        deviceIntake.update();
+
         colorSensors.forEach((k, v) -> {
             NormalizedRGBA color = v.getNormalizedColors();
             double distanceCM = v.getDistance(DistanceUnit.CM);
@@ -74,6 +86,8 @@ public class CalibrateColorSensors extends OpMode {
 
     @Override
     public void stop() {
+        deviceIntake.stop();
+
         telemetry.addData("status", "stopping");
         telemetry.update();
     }
